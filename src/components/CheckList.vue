f1d2721a06fc681b4d8aae0947cc1fdac5a7b83c
Jacob Bloemker <jbloemker97@gmail.com>
2019-08-08 20:54:08 -0400

    Initial commit

---
+++ src/components/CheckList.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="row mt-5">
+        <div class="col-lg-8">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Trade CheckList</h5>
+                    <ul class="list-group">
+                        <li class="list-group-item">Two previous tests of level on 30min (or greater) chart</li>
+                        <li class="list-group-item">Atleast one previous intraday rejection</li>
+                        <li class="list-group-item">Not a higher high or lower low</li>
+                        <li class="list-group-item">Not entering opposite side of momentum</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="text-center">Can I Enter A Trade?</h5>
+                    <img :src="imgUrl" class="rounded">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CheckList',
+    data () {
+        return {
+            imgUrl: 'https://via.placeholder.com/150'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
