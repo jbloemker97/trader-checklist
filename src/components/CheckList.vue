f9a3e6b73758151a8dc6e3512743ec1e1793ed33
Jacob Bloemker <jbloemker97@gmail.com>
2019-08-09 08:01:37 -0400

    Started checking list logic

--- src/components/CheckList.vue
+++ src/components/CheckList.vue
@@ -6,9 +6,9 @@
                     <h5 class="card-title">Trade CheckList</h5>
                     <ul class="list-group">
                         <li class="list-group-item">Two previous tests of level on 30min (or greater) chart</li>
-                        <li class="list-group-item">Atleast one previous intraday rejection</li>
+                        <li class="list-group-item">At least one previous intraday rejection</li>
                         <li class="list-group-item">Not a higher high or lower low</li>
-                        <li class="list-group-item">Not entering opposite side of momentum</li>
+                        <li class="list-group-item" data-is-checked="false" @click="trade">Not entering opposite side of momentum</li>
                     </ul>
                 </div>
             </div>
@@ -30,12 +30,24 @@ export default {
     name: 'CheckList',
     data () {
         return {
-            imgUrl: 'https://via.placeholder.com/150'
+            imgUrl: 'https://via.placeholder.com/150',
+            isTrade: false,
+        }
+    },
+    methods: {
+        trade: function (event) {
+            var dataCheck = event.target.getAttribute("data-is-checked");
+
+            event.target.setAttribute("data-is-checked", !!dataCheck);
+
+            console.log(dataCheck)
         }
     }
 }
 </script>
 
 <style scoped>
-
+    .line-through {
+        text-decoration: line-through;
+    }
 </style>
